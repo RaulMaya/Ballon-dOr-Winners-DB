85e5b749e6673b46695b5a8c53a8aa9cf0c8b7f4
Raul <raulmayas20@gmail.com>
2023-03-06 15:45:43 -0600

    Creating the server.js to call the necessary queries

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
-INSERT INTO winners (first_name, last_name, club, nation, age, win_year)
-VALUES ("Lionel", "Messi", "FC Barcelona", "Argentina", 23, 2010),
-       ("Cristiano", "Ronaldo", "Real Madrid FC", "Portugal", 27, 2013);
+INSERT INTO winners (first_name, last_name, club, nation, age)
+VALUES ("Lionel", "Messi", "FC Barcelona", "Argentina", 23),
+       ("Cristiano", "Ronaldo", "Real Madrid FC", "Portugal", 27),
+       ("Karim", "Benzema", "Real Madrid FC", "France", 33),
+       ("Ricardo", "'Kaka'", "AC Milan", "Brazil", 26);
