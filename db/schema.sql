0d1436750a51b0e9de29525f73f6590cb8a69c1d
Raul <raulmayas20@gmail.com>
2023-03-07 13:23:30 -0600

    Creating the tables in shcema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,33 @@
 DROP DATABASE IF EXISTS ballon_dor_db;
+
 CREATE DATABASE ballon_dor_db;
 
 USE ballon_dor_db;
 
 CREATE TABLE winners (
   year_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
   club VARCHAR(30) NOT NULL,
   nation VARCHAR(30) NOT NULL,
-  age INT NOT NULL
+  age INT NOT NULL,
+  coach VARCHAR(30)
 );
 
-ALTER TABLE winners AUTO_INCREMENT = 1995;
+ALTER TABLE
+  winners AUTO_INCREMENT = 1990;
+
+CREATE TABLE clubs (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  club VARCHAR(30),
+  club_rating INT FOREIGN KEY (club) REFERENCES winners(club) ON DELETE
+  SET
+    NULL
+);
+
+CREATE TABLE managers (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  manager_name VARCHAR(30) FOREIGN KEY (manager_name) REFERENCES winners(coach) ON DELETE
+  SET
+    NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,114 @@
-INSERT INTO winners (first_name, last_name, club, nation, age)
-VALUES ("Lionel", "Messi", "FC Barcelona", "Argentina", 23),
-       ("Cristiano", "Ronaldo", "Real Madrid FC", "Portugal", 27),
-       ("Karim", "Benzema", "Real Madrid FC", "France", 33),
-       ("Ricardo", "'Kaka'", "AC Milan", "Brazil", 26);
+DELETE FROM
+    winners;
+
+DELETE FROM
+    clubs;
+
+DELETE FROM
+    managers;
+
+INSERT INTO
+    winners (first_name, last_name, club, nation, age, coach)
+VALUES
+    (
+        "Lothar",
+        "Matthäus",
+        "Inter Milan",
+        "Germany",
+        28,
+        "Giovanni Trapattoni"
+    ),
+    (
+        "Jean-Pierre",
+        "Papin",
+        "Marseille",
+        "France",
+        27,
+        "Raymond Goethals"
+    ),
+    (
+        "Marco",
+        "van Basten",
+        "AC Milan",
+        "Netherlands",
+        28,
+        "Fabio Capello"
+    ),
+    (
+        "Roberto",
+        "Baggio",
+        "Juventus",
+        "Italy",
+        26,
+        "Giovanni Trapattoni"
+    ),
+    (
+        "Hristo",
+        "Stoichkov",
+        "Barcelona",
+        "Bulgaria",
+        28,
+        "Johan Cruyff"
+    ),
+    (
+        "George",
+        "Weah",
+        "AC Milan",
+        "Liberia",
+        29,
+        "Fabio Capello"
+    ),
+    (
+        "Matthias",
+        "Sammer",
+        "Borussia Dortmund",
+        "Germany",
+        29,
+        "Ottmar Hitzfeld"
+    ),
+    (
+        "Ronaldo",
+        "Nazario",
+        "Inter Milan",
+        "Brazil",
+        21,
+        "Luigi Simoni"
+    ),
+    (
+        "Zinedine",
+        "Zidane",
+        "Juventus",
+        "France",
+        26,
+        "Carlo Ancelotti"
+    ),
+    (
+        "Rivaldo",
+        null,
+        "Barcelona",
+        "Brazil",
+        27,
+        "Louis van Gaal"
+    );
+
+INSERT INTO
+    clubs (club, club_rating)
+VALUES
+    ("Inter Milan", 8),
+    ("Marseille", 7),
+    ("AC Milan", 9),
+    ("Juventus", 9),
+    ("Barcelona", 9),
+    ("Borussia Dortmund", 8);
+
+INSERT INTO
+    managers (manager_name)
+VALUES
+    ("Giovanni Trapattoni"),
+    ("Raymond Goethals"),
+    ("Fabio Capello"),
+    ("Johan Cruyff"),
+    ("Ottmar Hitzfeld"),
+    ("Luigi Simoni"),
+    ("Carlo Ancelotti"),
+    ("Louis van Gaal");
