3c327dcb93e943e4f500270721d9299e3a8b83a1
Raul <raulmayas20@gmail.com>
2023-03-08 23:28:54 -0600

    Hanlding cases where there is not a country available

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const db = require("./config/connection");
 const inquirer = require("inquirer");
 
-function execute_rows(query) {
+const execute_rows = (query) => {
   return new Promise((resolve, reject) => {
     db.query(query, function (err, result, fields) {
       if (err) {
@@ -12,7 +12,48 @@ function execute_rows(query) {
       resolve(result);
     });
   });
-}
+};
+
+const addNation = async () => {
+  const newNation = await inquirer.prompt([
+    {
+      type: "input",
+      name: "nation",
+      message: "Enter the new nationality that has won the ballon d'or.",
+      validate: async (answer) => {
+        const arrNation = answer.split(" ");
+        for (let i = 0; i < arrNation.length; i++) {
+          arrNation[i] =
+            arrNation[i].charAt(0).toUpperCase() + arrNation[i].slice(1);
+          const newCountry = arrNation.join(" ");
+          const currNations = await execute_rows("SELECT country FROM nations");
+          const nationsMap = currNations.map((country) => country.country);
+          if (nationsMap.includes(newCountry)) {
+            let index = nationsMap.indexOf(newCountry);
+            return `${newCountry} (${index}) is already in our database.`;
+          } else {
+            return true;
+          }
+        }
+      },
+    },
+  ]);
+
+  const arrNation = newNation.split(" ");
+  for (let i = 0; i < arrNation.length; i++) {
+    arrNation[i] =
+      arrNation[i].charAt(0).toUpperCase() + arrNation[i].slice(1);
+    const newCountry = arrNation.join(" ");
+  // Query database
+
+  db.query(
+    `INSERT INTO nations (country) VALUES (?);`,
+    newCountry,
+    function (err, results) {
+      console.log(`${newCountry} added to the database.`);
+    }
+  );
+};
 
 console.log(`  
   ___________________________
@@ -24,7 +65,9 @@ console.log(`
 '|_| |         |         | |_|'
  |___|         |         |___|
  |             |             |
- |_____________|_____________|`);
+ |_____________|_____________|
+ 
+ `);
 
 const myInq = async () => {
   const result = await inquirer.prompt([
@@ -43,6 +86,7 @@ const myInq = async () => {
         "Add winner",
         "Full Ballon D'Or winners",
         "Update winners team",
+        "Stats",
         "Exit",
       ],
     },
@@ -56,15 +100,11 @@ const myInq = async () => {
       myInq();
     });
   } else if (result["Introduction"] === "Add nation") {
-    const newNation = await inquirer.prompt([
-      {
-        type: "input",
-        name: "nation",
-        message:
-          "Enter the new nationality that has won the ballon d'or:",
-      },
-    ]);
-    console.log(newNation['nation']);
+    addNation();
+    db.query("SELECT * FROM nations", function (err, results) {
+      console.table(results);
+    });
+    myInq();
   } else if (result["Introduction"] === "View all clubs") {
     // Query database
     db.query(
@@ -74,6 +114,77 @@ const myInq = async () => {
         myInq();
       }
     );
+  } else if (result["Introduction"] === "Add club") {
+    const countryData = await execute_rows("SELECT country FROM nations;");
+    const clubCountryMap = countryData.map((c) => c.country);
+    clubCountryMap.push("Add Country");
+
+    const newClub = await inquirer.prompt([
+      {
+        type: "input",
+        name: "club",
+        message:
+          "Enter the club that has/had a player who won the ballon d'or with that team.",
+      },
+      {
+        type: "number",
+        name: "clubRating",
+        message: "From 1 to 10, please rate this team.",
+        validate: (answer) => {
+          if (typeof answer === "number") {
+            if (answer < 0 || answer > 10) {
+              return "Please enter a number from 1 to 10.";
+            }
+            return true;
+          } else {
+            return "Please enter a number.";
+          }
+        },
+      },
+      {
+        type: "list",
+        name: "clubCountry",
+        message: "Select the country of origin of the club.",
+        choices: clubCountryMap,
+      },
+    ]);
+
+    if (newClub["clubCountry"] === "Add Country") {
+      addNation();
+    }
+    let countryId = clubCountryMap.indexOf(newClub["clubCountry"]);
+
+    const arrClub = newClub["club"].split(" ");
+    for (let i = 0; i < arrClub.length; i++) {
+      arrClub[i] = arrClub[i].charAt(0).toUpperCase() + arrClub[i].slice(1);
+    }
+
+    const newTeam = arrClub.join(" ");
+    // Query database
+    const currClubs = await execute_rows("SELECT club FROM clubs");
+    const clubMap = currClubs.map((club) => club.club);
+    if (clubMap.includes(newTeam)) {
+      let index = clubMap.indexOf(newTeam);
+      console.log(`${newTeam} (${index}) is already in our database.`);
+      myInq();
+    } else {
+      const clubValues = [newTeam, newClub["clubRating"], countryId + 1];
+      db.query(
+        `INSERT INTO clubs (club, club_rating, country_id) VALUES (?);`,
+        [clubValues],
+        function (err, results) {
+          if (err) throw err;
+          console.log(`${newTeam} added to the database.`);
+        }
+      );
+      db.query(
+        "SELECT club, club_rating, country as clubs_country FROM clubs JOIN nations ON clubs.country_id = nations.id;",
+        function (err, results) {
+          console.table(results);
+          myInq();
+        }
+      );
+    }
   } else if (result["Introduction"] === "View all managers") {
     // Query database
     db.query(
